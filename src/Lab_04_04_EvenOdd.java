e55806283932c2f5ed2cf18d56ab6b18eee4b563
Deven Seth <devenseth8@gmail.com>
2023-02-05 11:52:39 -0500

    variable name assignment

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,14 @@
+public class Lab_04_04_EvenOdd {
+
+    public static void main(String[] args) {
+        //variables
+        int input = 10;
+
+        //Output
+        if (input % 2 == 0){
+            System.out.println("The number " + input + " is even");
+        } else {
+            System.out.println("The number " + input + "is odd");
+        }
+    }
+}
